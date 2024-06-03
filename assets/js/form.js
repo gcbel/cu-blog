ae6cd7d210aea67668952685f5ab833f07b5b16e
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-06-03 15:07:55 -0700

    add form submission to local storage, link htmls

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,71 +1,77 @@
+/* Global variables */
+const submit = document.querySelector('.submit');
+const nameRequired = document.getElementById("name-required");
+const titleRequired = document.getElementById("title-required");
+const postRequired = document.getElementById("post-required")
 
 /* Submitting the form */
-const submit = document.querySelector('.submit');
 submit.addEventListener('click', function(event) {
     event.preventDefault();
 
     // Fetch local data and revert to an array
     let stringOfEntries = localStorage.getItem("entries");
-    console.log(stringOfEntries)
     let entries = [];
 
     // Check if entriesString is not null or empty
     if (stringOfEntries) {
         try {
-            entries = JSON.parse(entriesString);
+            entries = JSON.parse(stringOfEntries);
         } catch (e) {
             console.log("Your first blog entry!");
         }
     }
 
     // Get user input
-    const name = document.getElementById("name").value;
-    const postTitle = document.getElementById("post-title").value;
-    const postContent = document.getElementById("post-content").value;
+    let name = document.getElementById("name");
+    const postTitle = document.getElementById("post-title");
+    const postContent = document.getElementById("post-content");
 
     // Check to ensure all input areas have content
-    let formCompleted = checkCompleted(name, postTitle, postContent);
+    let formCompleted = checkCompleted(name.value, postTitle.value, postContent.value);
     if (!formCompleted) {
         return;
     }
 
     // If form is completed, add user input and log to local storage
     const entry = {
-        username: name,
-        title: postTitle,
-        content: postContent,
+        username: name.value,
+        title: postTitle.value,
+        content: postContent.value,
     };
 
-    console.log(entry);
+    // Add to entries and clear values
     entries.push(entry);
-    console.log(entries);
-    console.log(JSON.stringify(entries));
     localStorage.setItem("entries", JSON.stringify(entries));
-    console.log(localStorage.getItem("entries"));
+    name.value = "";
+    postTitle.value = "";
+    postContent.value = "";
+
+    // Navigate to blog
+    window.location.href = "blog.html";
 })
 
 /* Check to ensure all input areas have content */
 function checkCompleted (name, postTitle, postContent) {
     let formCompleted = true;
     if (name.length === 0) {
-        document.getElementById("name-required").style.display = 'inline';
+        nameRequired.style.display = 'inline';
         formCompleted = false;
     } else {
-        document.getElementById("name-required").style.display = 'none';
+        nameRequired.style.display = 'none';
     }
     
     if (postTitle.length === 0) {
-        document.getElementById("title-required").style.display = 'inline';
+        titleRequired.style.display = 'inline';
         formCompleted = false;
     } else {
-        document.getElementById("title-required").style.display = 'none';
+        titleRequired.style.display = 'none';
     }
     
     if (postContent.length === 0) {
-        document.getElementById("post-required").style.display = 'inline';
+        postRequired.style.display = 'inline';
         formCompleted = false;
     } else {
-        document.getElementById("post-required").style.display = 'none';
+        postRequired.style.display = 'none';
     }
     return formCompleted;
 }
